1a62bd84760a943f5dbd17af430b629bd9872713
vex <vex@arch.com>
2024-03-04 09:56:49 +0200

    feature/log

--- source/irc-server/main.cpp
+++ source/irc-server/main.cpp
@@ -1,8 +1,27 @@
+#include <filesystem>
 #include <server.hpp>
+#include <xlog.hpp>
 
-int main() 
+namespace
 {
-    Server server("4000");
-    
-    return 0;
+	/*
+		set the current path to where the executable is located
+		in case someone starts the server from a different path
+		it should be still able to load its files w/o loss
+	*/
+	void initialize_working_directory(std::string passed_path)
+	{
+		std::filesystem::path working_path(passed_path);
+		std::filesystem::current_path(working_path.parent_path());
+	}
+}
+
+int main(int argc, char* args[])
+{
+	initialize_working_directory(args[0]);
+	xlog::initialize();
+
+	Server server("4000");
+
+	return 0;
 }
