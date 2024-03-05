adb8d4e5a0736b2ce4e48afe21d642444ccd19df
vex <vex@arch.com>
2024-03-05 04:32:28 +0200

    shorten the code with std::forward

--- source/irc-server/include/xlog.hpp
+++ source/irc-server/include/xlog.hpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <iostream>
 #include <sstream>
+#include <utility>
 
 namespace xlog
 {
@@ -21,10 +22,15 @@ namespace xlog
     /* initializes the logging module */
     bool initialize();
 
-    template<typename... Args>
-    void output(const char* format, const Args&... args)
+    template<class... Args>
+    inline void stream_log_lambda(
+        std::fstream& stream,
+        std::mutex& mutex,
+        const char* format,
+        const Args&&... args
+        )
     {
-        xlog::output_mutex.lock();
+        mutex.lock();
         
         auto time_now { std::chrono::system_clock::now() };
         /* time_now.time_since_epoch().count() returns the time in nanoseconds */
@@ -38,75 +44,33 @@ namespace xlog
             << std::vformat(format, std::make_format_args(args...)) << std::endl;
 
         std::string message { ss.str() };
-        xlog::output_stream << message;
+        stream << message;
         std::cout << message;
-        xlog::output_mutex.unlock();
+        mutex.unlock();
+    }
+
+    template<class... Args>
+    void output(const char* format, const Args&&... args)
+    {
+        xlog::stream_log_lambda(xlog::output_stream, xlog::output_mutex, format, std::forward<Args>(args)...);
     }
 
     template<typename... Args>
     inline void message(const char* format, const Args&... args)
     {
-        xlog::messages_mutex.lock();
-        
-        auto time_now { std::chrono::system_clock::now() };
-        /* time_now.time_since_epoch().count() returns the time in nanoseconds */
-        auto time_ms { (time_now.time_since_epoch().count() / 1000000) % 1000 };
-        auto time_t { std::chrono::system_clock::to_time_t(time_now) };
-        auto now_tm { *std::localtime(&time_t) };
-
-        std::stringstream ss;
-        ss << std::put_time(&now_tm, "%d-%m-%y %H:%M:%S")
-            << "." << time_ms << " " 
-            << std::vformat(format, std::make_format_args(args...)) << std::endl;
-
-        std::string message { ss.str() };
-        xlog::messages_stream << message;
-        std::cout << message;
-        xlog::messages_mutex.unlock();
+        xlog::stream_log_lambda(xlog::messages_stream, xlog::messages_mutex, format, std::forward<Args>(args)...);
     }
 
     template<typename... Args>
     inline void error(const char* format, const Args&... args)
     {
-        xlog::error_mutex.lock();
-        
-        auto time_now { std::chrono::system_clock::now() };
-        /* time_now.time_since_epoch().count() returns the time in nanoseconds */
-        auto time_ms { (time_now.time_since_epoch().count() / 1000000) % 1000 };
-        auto time_t { std::chrono::system_clock::to_time_t(time_now) };
-        auto now_tm { *std::localtime(&time_t) };
-
-        std::stringstream ss;
-        ss << std::put_time(&now_tm, "%d-%m-%y %H:%M:%S")
-            << "." << time_ms << " " 
-            << std::vformat(format, std::make_format_args(args...)) << std::endl;
-
-        std::string message { ss.str() };
-        xlog::error_stream << message;
-        std::cout << message;
-        xlog::error_mutex.unlock();
+        xlog::stream_log_lambda(xlog::error_stream, xlog::error_mutex, format, std::forward<Args>(args)...);
     }
 
     template<typename... Args>
     inline void access(const char* format, const Args&... args)
     {
-        xlog::access_mutex.lock();
-        
-        auto time_now { std::chrono::system_clock::now() };
-        /* time_now.time_since_epoch().count() returns the time in nanoseconds */
-        auto time_ms { (time_now.time_since_epoch().count() / 1000000) % 1000 };
-        auto time_t { std::chrono::system_clock::to_time_t(time_now) };
-        auto now_tm { *std::localtime(&time_t) };
-
-        std::stringstream ss;
-        ss << std::put_time(&now_tm, "%d-%m-%y %H:%M:%S")
-            << "." << time_ms << " " 
-            << std::vformat(format, std::make_format_args(args...)) << std::endl;
-
-        std::string message { ss.str() };
-        xlog::access_stream << message;
-        std::cout << message;
-        xlog::access_mutex.unlock();
+        xlog::stream_log_lambda(xlog::access_stream, xlog::access_mutex, format, std::forward<Args>(args)...);
     }
 }
 
